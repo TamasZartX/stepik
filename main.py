610077f68c46cd5a91d9bbb42186109787902617
Tamas ZartX <tamaszartx@gmail.com>
2025-02-25 06:43:53 +0300

    test2

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("omg it worked")
-print("and it keeps doing it")
+import logging.config
 
+from logging_settings import logging_config
+from module_1 import main
+
+# Загружаем настройки логирования из словаря `logging_config`
+logging.config.dictConfig(logging_config)
+
+# Исполняем функцию `main` из модуля `module_1.py`
+main()
